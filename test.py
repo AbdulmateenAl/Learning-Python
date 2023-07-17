cd424487328202d8b59d864e28b7300580aa0f54
AbdulmateenAl <alabiabdulmateen94@gmail.com>
2023-07-17 23:36:51 +0100

    Added some new files

--- test.py
+++ test.py
@@ -1,1 +1,8 @@
-print(chr(97))
+string = "1.2.3.4"
+i = 0
+for i in range(len(string)):
+    if string[i] == ".":
+        string = string[:i] + string[i+1:]
+        print(string)
+    else:
+        print(string)
