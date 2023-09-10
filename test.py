46be22f5cc8eba6499ed2571fe3fdb298a95f097
AbdulmateenAl <alabiabdulmateen94@gmail.com>
2023-09-10 04:06:13 +0100

    Testing random codes

--- test.py
+++
@@ -1,8 +0,0 @@
-string = "1.2.3.4"
-i = 0
-for i in range(len(string)):
-    if string[i] == ".":
-        string = string[:i] + string[i+1:]
-        print(string)
-    else:
-        print(string)
